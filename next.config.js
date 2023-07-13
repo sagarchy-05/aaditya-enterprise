dc5deb5182c53aba5829abf44e95c41eaa7ad214
Sagar Kumar Choudhary <sagarchoudharyaot@gmail.com>
2023-07-13 18:03:27 +0530

    changed nextconfig.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  distDir: 'build', //this line will tell the build to create a file with this name
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
